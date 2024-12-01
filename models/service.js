3522783d10823bb72ab8e8748757abd0d719c8fb
BrunoRaya <brunoraya00@gmail.com>
2024-12-01 17:37:16 -0300

    changes 4

--- models/service.js
+++ models/service.js
@@ -1,15 +1,18 @@
 import mongoose from 'mongoose';
 
 const serviceSchema = new mongoose.Schema({
-  id_user: { type: String, required: true },
-  id_prof: { type: String, required: true },
-  valor: { type: Number, required: true },
-  hora: { type: Date, required: true },
-  loc: { type: String, required: true },
-  status: { type: String, required: true },
-  descricao: { type: String, required: true },
-});
+    id_user: { type: String, required: true },
+    id_prof: { type: String, required: true },
+    valor: { type: Number, required: true },
+    hora: { type: Date, required: true },
+    loc: { type: String, required: true },
+    status: { type: String, required: true },
+    descricao: { type: String, required: true },
+  });
 
-const Service = mongoose.model('Service', serviceSchema, 'servicos');
-
-export default Service;
+  serviceSchema.index({ id_user: 1, id_prof: 1, hora: 1 }, { unique: true });
+  
+  const Service = mongoose.model('Service', serviceSchema, 'servicos');
+  
+  export default Service;
+  
